f90726ab743d6e6de80018845149c5889bde69ab
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-01-31 17:00:38 +0700

    JSX to JavaScript and Back
    JSX to JavaScript and Back

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
+    // ตัวแปร
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    // logic
+    if(hours < 12){
+        timeOfDay = "morning"
+    }else if(hours >= 12 && hours < 17){
+        timeOfDay = "afternoon"
+    }else{
+        timeOfDay = "night";
+    }
+    // return 
     return (
-        <h1>Hello world</h1>
+    <h1>Good {timeOfDay} !</h1>
     )
 }
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+// render function <App/>
+ReactDOM.render(<App />, document.getElementById("root"));
