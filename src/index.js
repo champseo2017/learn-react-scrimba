a1302a7eafc2d10e22b07bc7bf4f9d1b1f67f7b3
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-01-31 17:20:51 +0700

    React Inline Styles with the Style Property
    React Inline Styles with the Style Property

--- src/index.js
+++ src/index.js
@@ -2,22 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-    // ตัวแปร
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay;
-    // logic
-    if(hours < 12){
-        timeOfDay = "morning"
-    }else if(hours >= 12 && hours < 17){
-        timeOfDay = "afternoon"
-    }else{
-        timeOfDay = "night";
-    }
-    // return 
-    return (
-    <h1>Good {timeOfDay} !</h1>
-    )
+  // ตัวแปร
+  const date = new Date(2020, 1, 31, 15);
+  const hours = date.getHours();
+  let timeOfDay;
+  const styles = {
+    fontSize: 30,
+  };
+  // logic
+  if (hours < 12) {
+    timeOfDay = "morning";
+    styles.color = "#04756F"
+    Object.assign({}, styles, styles.color)
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+    styles.color = "#8914A3"
+    Object.assign({}, styles, styles.color)
+  } else {
+    timeOfDay = "night";
+    styles.color = "#D90000"
+    Object.assign({}, styles, styles.color)
+  }
+
+  // return
+  return <h1 style={styles}>Good {timeOfDay} !</h1>;
 }
 // render function <App/>
 ReactDOM.render(<App />, document.getElementById("root"));
