602bf8a85c84d39034347d8de2c91d0498f46761
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-03 15:10:02 +0700

    Mapping Components Practice 2
    Mapping Components Practice 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
-import Joke from "./Joke";
-import jokesData from "./jokesData";
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findindex
+import Product from "./Product"
+import productsData from "./vschoolProducts";
 
 function App() {
-  const jokeComponents = jokesData.map(joke => {
-      return <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine}/>
-  });
+  const productComponents = productsData.map(item => <Product key={item.id} product={item}/>)
   return (
     <div>
-      {jokeComponents}
+      {productComponents}
     </div>
   )
 }
+
 export default App;
