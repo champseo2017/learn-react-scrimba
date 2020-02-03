6c2a9f38b6708aa789efec3e9bbace839a6f5a13
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-03 11:46:58 +0700

    React Props
    React Props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React from "react";
-import TodoItem from './TodoItem'
+import ContactCard from "./ContactCard";
+
 function App() {
-    return (
-        <div className="todo-list">
-           <TodoItem/>
-           <TodoItem/>
-           <TodoItem/>
-           <TodoItem/>
-        </div>
-    )
+  return (
+    <div className="contacts">
+      <ContactCard
+        contact={{
+          name: "Mr. Whiskerson",
+          imgUrl: "http://placekitten.com/300/200",
+          phone: "(212) 555-1234",
+          email: "mr.whiskaz@catnap.meow"
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: "Fluffykins",
+          imgUrl: "http://placekitten.com/400/200",
+          phone: "(212) 555-2345",
+          email: "fluff@me.com"
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: "Destroyer",
+          imgUrl: "http://placekitten.com/400/300",
+          phone: "(212) 555-3456",
+          email: "ofworlds@yahoo.com"
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: "Felix",
+          imgUrl: "http://placekitten.com/200/100",
+          phone: "(212) 555-4567",
+          email: "thecat@hotmail.com"
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
