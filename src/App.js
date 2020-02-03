60e6026b256037d6347f5aca76ede1ec82f34f76
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-03 10:18:56 +0700

    react to app phas 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent"
-import Footer from "./components/Footer";
-
+import TodoItem from './TodoItem'
 function App() {
     return (
-        <div>
-            <Header/>
-            <MainContent/>
-            <Footer/>
+        <div className="todo-list">
+           <TodoItem/>
+           <TodoItem/>
+           <TodoItem/>
+           <TodoItem/>
         </div>
     )
 }
-export default  App;
+
+export default App;
