3b312477ae692903b43e4956dde9336a26da31ab
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-03 14:46:39 +0700

    Mapping Components in React
    Mapping Components in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
 import Joke from "./Joke";
+import jokesData from "./jokesData";
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findindex
+
 function App() {
+  const jokeComponents = jokesData.map(joke => {
+      return <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine}/>
+  });
   return (
     <div>
-      <Joke
-        punchLine="I don't know, but the flag is a big plus!"
-      />
-      <Joke
-         question="Did you hear about the mathematician who's afraid of negative numbers?"
-         punchLine="He'll stop at nothing to avoid them!"
-      />
-      <Joke
-         question="Hear about the new restaurant called Karma?"
-         punchLine="There’s no menu: You get what you deserve."
-      />
-      <Joke
-         question="Did you hear about the actor who fell through the floorboards?"
-         punchLine="He was just going through a stage."
-      />
-    
+      {jokeComponents}
     </div>
   )
 }
