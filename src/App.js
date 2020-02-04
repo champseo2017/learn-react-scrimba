9269a2d64cdea84f99d3c59652156424e0e993dd
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-04 15:22:59 +0700

    React setState: Changing the State
    React setState: Changing the State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
-import TodoItem from "./TodoItem";
-import todosData from "./todosData";
-
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: todosData
+      count: 0
     };
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick () {
+     this.setState((prevState) => {
+        return {
+          count: prevState.count + 1
+        }
+     })
   }
   render() {
-    const todoItems = this.state.todos.map(item => (
-      <TodoItem key={item.id} item={item} />
-    ));
-    return <div className="todo-list">{todoItems}</div>;
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+        <ChildComponent count={this.state.count}/>
+      </div>
+    );
   }
 }
 
