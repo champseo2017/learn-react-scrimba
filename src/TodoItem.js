1a25073b07973f39c456119914525833da607723
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-04 14:57:10 +0700

    React Todo App: Phase 5
    React Todo App: Phase 5

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,13 @@ import React from "react";
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props && props.item.completed} />
+      <input
+        type="checkbox"
+        checked={props && props.item.completed}
+        onChange={() => {
+          console.log("Changed!");
+        }}
+      />
       <p>{props && props.item.text}</p>
     </div>
   );
