0a3856dd6231298d67a7fe1e24faea07bf7e1fd6
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-07 17:48:22 +0700

    tmt online and offline test logic 1
    tmt online and offline test logic 1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       loading: false,
       character: []
     };
+    this.onchangeonline = this.onchangeonline.bind(this);
   }
   componentDidMount() {
     this.setState({ loading: true });
@@ -15,23 +16,44 @@ class App extends Component {
       .then(response => response.json())
       .then(data => {
         this.setState({
-          loading:false,
+          loading: false,
           character: data
         });
       });
   }
+  onchangeonline(e) {
+    // save data ofline
+    if (e === false) {
+      console.log(this.state.character);
+      console.log("save data ofline ");
+    } else {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        });
+      console.log("send data online");
+    }
+  }
   render() {
-   
-    const text = this.state.loading ? <h1>loading...</h1> :  this.state.character.map((value,key) => {
-    return <p key={key}>Title: {value.title}</p>
-    });
+    const text = this.state.loading ? (
+      <h1>loading...</h1>
+    ) : (
+      this.state.character.map((value, key) => {
+        return <p key={key}>Title: {value.title}</p>;
+      })
+    );
     return (
       <div>
         {/* 
           data online -> props(dataonline) -> component offline (props(data online)) -> sends to redux or component any
         */}
-        <Online polling={false}><h1>Online: Data</h1> {text}</Online>
-        <Offline polling={false}><h1>Offline: Data not refresh pages</h1> {text}</Offline>
+        <Online onChange={this.onchangeonline} polling={false}>
+          <h1>Online: Data</h1> {text}
+        </Online>
+        <Offline polling={false}>
+          <h1>Offline: Data not refresh pages</h1> {text}
+        </Offline>
       </div>
     );
   }
