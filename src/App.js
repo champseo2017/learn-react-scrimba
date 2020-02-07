a780df9cb96d7039fbf601e74b0877c5f2b3d585
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-07 14:07:48 +0700

    React Todo App: Phase 7
    React Todo App: Phase 7

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
 import React from "react";
-
-/* 
-Challenge:
-
-Given a stateless functional component:
-1.Follow the steps necessary to add state to it,
- - class-based component
- - constructor method
-2.Have state keep track of whether the user is logged in or not
- - isLoggedIn: Boolean (true or false)
-3.Add a button that logs the user in/out
-  - event listener (onClick)
-  a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
-   // Conditional Rendering
-
-4.Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not
-// Conditional Rendering 
-
-*/
+import TodoItem from "./TodoItem";
+import todosData from "./todosData";
 
 class App extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      isLoggedIn: false
-    };
-    this.handleClick = this.handleClick.bind(this);
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
-  handleClick() {
+  handleChange(id){
     this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if(todo.id === id){
+            todo.completed = !todo.completed
+        }
+        return todo
+      })
       return {
-        isLoggedIn: !prevState.isLoggedIn
-      };
-    });
+        todos: updatedTodos
+      }
+    })
   }
+
   render() {
-    let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN";
-    let displayText = this.state.isLoggedIn ? "Logged in" : "Logged out";
+    const todoItems = this.state.todos.map (item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
     return (
-      <div>
-        <button onClick={this.handleClick}>{buttonText}</button>
-        <h1>{displayText}</h1>
+      <div className="todo-list">
+        {todoItems}
       </div>
-    );
+    )
   }
 }
 
