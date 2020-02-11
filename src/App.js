3cb2994567c8da5133da3e23a480e97cc6427223
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-11 10:32:39 +0700

    React Forms Part 1
    React Forms Part 1

--- src/App.js
+++ src/App.js
@@ -1,62 +1,53 @@
 import React, { Component } from "react";
-import { Offline, Online } from "react-detect-offline";
-// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      loading: false,
-      character: []
+      firstName: "",
+      lastName: ""
     };
-    this.onchangeonline = this.onchangeonline.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  componentDidMount() {
-    this.setState({ loading: true });
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          loading: false,
-          character: data
-        });
-      });
-  }
-  onchangeonline(e) {
-    // save data ofline
-    if (e === false) {
-      console.log(this.state.character);
-      console.log("save data ofline ");
-    } else {
-      fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-        });
-      console.log("send data online");
-    }
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
   }
   render() {
-    const text = this.state.loading ? (
-      <h1>loading...</h1>
-    ) : (
-      this.state.character.map((value, key) => {
-        return <p key={key}>Title: {value.title}</p>;
-      })
-    );
     return (
-      <div>
-        {/* 
-          data online -> props(dataonline) -> component offline (props(data online)) -> sends to redux or component any
-        */}
-        <Online onChange={this.onchangeonline} polling={false}>
-          <h1>Online: Data</h1> {text}
-        </Online>
-        <Offline polling={false}>
-          <h1>Offline: Data not refresh pages</h1> {text}
-        </Offline>
-      </div>
+      <form>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          onChange={this.handleChange}
+          value={this.state.firstName}
+          autoComplete="off"
+        />
+        <br />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          onChange={this.handleChange}
+          value={this.state.lastName}
+          autoComplete="off"
+        />
+        <h1>
+          {this.state.firstName} {this.state.lastName}
+        </h1>
+      </form>
     );
   }
 }
 
 export default App;
+
+/* 
+React Docs about Forms:
+https://reactjs.org/docs/forms.html
+
+
+*/
