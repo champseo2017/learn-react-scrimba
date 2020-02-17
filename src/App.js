8031a789af6587f52cea814c36e2ecca89c3a17d
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-17 15:12:18 +0700

    React Form Part 2
    React Form Part 2

--- src/App.js
+++ src/App.js
@@ -1,53 +1,96 @@
 import React, { Component } from "react";
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
       firstName: "",
-      lastName: ""
+      lastName: "",
+      isFriendly: false,
+      gender: "",
+      favColor: "blue"
     };
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
+    const { name, value, type, checked } = event.target;
+    type === "checkbox"
+      ? this.setState({ [name]: checked })
+      : this.setState({ [name]: value });
   }
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input
           type="text"
+          value={this.state.firstName}
           name="firstName"
           placeholder="First Name"
           onChange={this.handleChange}
-          value={this.state.firstName}
-          autoComplete="off"
         />
         <br />
         <input
           type="text"
+          value={this.state.lastName}
           name="lastName"
           placeholder="Last Name"
           onChange={this.handleChange}
-          value={this.state.lastName}
-          autoComplete="off"
-        />
+        />{" "}
+        <br />
+        <textarea onChange={this.handleChange} value={"Some default value"} />
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="isFriendly"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
+          />{" "}
+          Is friendly
+        </label>
+        <br />
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={this.state.gender === "male"}
+            onChange={this.handleChange}
+          />{" "}
+          Male
+        </label>
+        <br />
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={this.state.gender === "female"}
+            onChange={this.handleChange}
+          />{" "}
+          Female
+        </label>
+        {/* Formik */}
+        <br />
+        <select
+          value={this.state.favColor}
+          onChange={this.handleChange}
+          name="favColor"
+        >
+          <option value="blue">Blue</option>
+          <option value="green">Green</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+        </select>
         <h1>
           {this.state.firstName} {this.state.lastName}
         </h1>
+        <h2>You are a {this.state.gender}</h2>
+        <h2>Your favorite color is {this.state.favColor}</h2>
+        <button>Submit</button>
       </form>
     );
   }
 }
 
 export default App;
-
-/* 
-React Docs about Forms:
-https://reactjs.org/docs/forms.html
-
-
-*/
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,20 +1,20 @@
-import React from "react";
-function TodoItem(props) {
-  const completedStyle = {
+import React from "react"
+function TodoItem(props){
+  const completedStyled = {
     fontStyle: "italic",
     color: "#cdcdcd",
     textDecoration: "line-through"
   }
   return (
     <div className="todo-item">
-      <input
-        type="checkbox"
-        checked={props.item.completed}
-        onChange={() => props.handleChange(props.item.id)}
-      />
-      <p style={props.item.completed ? completedStyle:null}>{props.item.text}</p>
+        <input
+          type="checkbox"
+          checked={props.item.completed}
+          onChange={()=> props.handleChange(props.item.id)}
+        />
+        <p style={props.item.completed ? completedStyled:null}>{props.item.text}</p>
     </div>
-  );
+  )
 }
 
-export default TodoItem;
+export default TodoItem
