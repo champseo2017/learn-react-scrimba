2d8c52d17d942e57c28e808473062bbbc96fd6d7
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-19 15:18:14 +0700

    useEffect() Part 1
    useEffect() Part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React, {useState} from "react";
+/* 
+ componentDidMount
+ componentDidUpdate
+ componentWillUnmount
+*/
+// Side effects?
+// Network request
+// Manual DOM manipulatton
+// Event listeners or timeouts and intervals
 
+import React, { useState, useEffect } from "react";
+import randomcolor from "randomcolor";
 function App() {
   const [count, setCount] = useState(0);
-  const [answer, setAnswer] = useState("Yes");
-
+  const [color, setColor] = useState(""); // componentDidMount | initial state | value default
   function increment() {
-    setCount(prevCount => prevCount + 1)
+    setCount(prevCount => prevCount + 1);
   }
-
   function decrement() {
-    setCount(prevCount => prevCount - 1)
+    setCount(prevCount => prevCount - 1);
   }
 
+  useEffect(() => {
+      setColor(randomcolor())
+  },[count]); // [count, otherVars]
+  // state change component render (run function)
+
+  /* useEffect
+  useEffect({}) state change component render (run function) (componentDidUpdate)
+  componentDidUpdate (state change run useEffect) -> componentDidMount(DidMount ->state -> dom)
+  componentWillUnmount
+
+  2. Didmount only (componentDidMount)
+  useEffect(() => {
+    setColor(randomcolor())
+  },[])
+  componentWillUnmount
+  */
+  
   return (
     <div>
-        <h1>{count}</h1>
+        <h1 style={{color: color}}>{count}</h1>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
