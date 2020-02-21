e38769302421146f4c7d39d1b66de79ea0e65a66
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-21 11:31:51 +0700

    React Hooks useState - Part 2
    React Hooks useState - Part 2

--- src/Playground.js
+++ src/Playground.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import randomColor from "randomcolor";
-
 export default function Playground() {
   const [count, setCount] = useState(0);
   return (
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from "./App"
 import Playground from "./Playground"
-ReactDOM.render(<Playground/>, document.getElementById('root'));
+import Paint from "./components/Paint"
+ReactDOM.render(<Paint/>, document.getElementById('root'));
