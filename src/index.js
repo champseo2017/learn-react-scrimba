679d06c8498d65ac4ad9b32daecd459a006debc0
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-02-21 13:32:21 +0700

    React Hooks useEffec
    React Hooks useEffec

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 import App from "./App"
 import Playground from "./Playground"
 import Paint from "./components/Paint"
-ReactDOM.render(<Paint/>, document.getElementById('root'));
+ReactDOM.render(<Playground/>, document.getElementById('root'));
